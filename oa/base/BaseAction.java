ad78e0ab6f91eab20337a21fae29061ee85d298b
Herbbbb <2322915188@qq.com>
2017-12-19 15:31:19 +0800

    version 1
    第一次提交

---
+++ oa/base/BaseAction.java
@@ -0,0 +1,58 @@
+package cn.itcast.oa.base;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import javax.annotation.Resource;
+
+import cn.itcast.oa.service.IBookService;
+import cn.itcast.oa.service.IDepartmentService;
+import cn.itcast.oa.service.IRoleService;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.opensymphony.xwork2.util.ValueStack;
+
+/**
+ * 通用父类Action
+ * @author zhaoqx
+ *
+ */
+public class BaseAction<T> extends ActionSupport implements ModelDriven<T>{
+	@Resource
+	protected IBookService bookService;
+	@Resource
+	protected IRoleService roleService;
+	@Resource
+	protected IDepartmentService departmentService;
+	
+	//在构造方法中获得model类型
+	public BaseAction(){
+		ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();//父类Action类型
+		Type[] types = type.getActualTypeArguments();
+		Class<T> clazz = (Class<T>) types[0];
+		try {
+			model = clazz.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected T model;
+
+	public T getModel() {
+		return model;
+	}
+	
+	/**
+	 * 获得值栈
+	 * @return
+	 */
+	protected ValueStack getValueStack(){
+		return ActionContext.getContext().getValueStack();
+	}
+	
+}
